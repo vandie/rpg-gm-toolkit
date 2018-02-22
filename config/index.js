3341d7a2a5520717ad5b17c49b47c4305f25f63a
Michael Van Der Velden <mikevdv@vandie.co.uk>
2018-02-22 14:26:01 +0000

    New build

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/RPG-Lore-Builder/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/RPG-Lore-Builder/',
 
     /**
      * Source Maps
