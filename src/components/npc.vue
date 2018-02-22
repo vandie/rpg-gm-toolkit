726157c525676253bd4558eb58b12126dcb864cd
Michael Van Der Velden <mikevdv@vandie.co.uk>
2018-02-22 17:02:21 +0000

    Add regenerate button

--- src/components/npc.vue
+++ src/components/npc.vue
@@ -4,17 +4,20 @@
           <div class="section">
             <div class="card">
                 <div class="card-content">
-                    <b-field label="Race">
-                        <b-select placeholder="No race selected" v-model="selRace">
-                            <option v-for="(race, index) in races" v-bind:key="index" v-bind:value="index">{{race.name}}</option>
-                        </b-select>
+                    <b-field grouped>
+                        <b-field label="Race">
+                            <b-select placeholder="No race selected" v-model="selRace">
+                                <option v-for="(race, index) in races" v-bind:key="index" v-bind:value="index">{{race.name}}</option>
+                            </b-select>
+                        </b-field>
+                        <b-field label="Sex">
+                            <b-select placeholder="No sex selected" v-model="sex">
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </b-select>
+                        </b-field>
                     </b-field>
-                    <b-field label="Sex">
-                        <b-select placeholder="No sex selected" v-model="sex">
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                        </b-select>
-                    </b-field>
+                    <a id="regen" class="button is-primary is-outlined" v-on:click="regenerate()">regenerate</a>
                 </div>
             </div>
           </div>
@@ -49,6 +52,12 @@ export default {
           sex: "male"
       }
   },
+  methods: {
+      regenerate: function(){
+          this.sex = this.sex == "male" ? "female" : "male"
+          this.sex = this.sex == "male" ? "female" : "male"
+      }
+  },
   computed:{
       currentRace: function(){
           return this.races[this.selRace]
@@ -79,7 +88,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+    #regen{
+        width:100%;;
+    }
 </style>
 
 
