6d86e98ecd6e3875b034be84e9aef729cad29001
Michael Van Der Velden <mikevdv@vandie.co.uk>
2018-02-22 17:37:54 +0000

    Add Class to character

--- src/components/npc.vue
+++ src/components/npc.vue
@@ -17,6 +17,14 @@
                             </b-select>
                         </b-field>
                     </b-field>
+                    <b-field grouped>
+                        <b-field label="Class">
+                            <b-select placeholder="No class selected" v-model="selClass">
+                                <option v-for="(c, index) in classes" v-bind:key="index" v-bind:value="index">{{c.name}}</option>
+                                <option v-bind:value="classes.length">Random Class</option>
+                            </b-select>
+                        </b-field>
+                    </b-field>
                     <a id="regen" class="button is-primary is-outlined" v-on:click="regenerate()">Regenerate</a>
                 </div>
             </div>
@@ -33,6 +41,8 @@
                     <h4> Sex : {{toNameCase(sex)}} </h4>
                     <h4> Height : {{stature.writtenHeight}} </h4>
                     <h4> Weight : {{stature.writtenWeight}} </h4>
+                    <br>
+                    <h4> Class : {{currentClass.name}} </h4>
                 </div>
             </div>
           </div>
@@ -42,13 +52,16 @@
 
 <script>
 import raceData from "../assets/data/raceData.json"
+import classData from "../assets/data/classData.json"
 
 export default {
   name:"NPC",
   data: function(){
       return {
           races: raceData.races,
+          classes: classData.classes,
           selRace: 0,
+          selClass: classData.classes.length,
           sex: "male"
       }
   },
@@ -56,37 +69,47 @@ export default {
     regenerate: function(){
         this.sex = this.sex == "male" ? "female" : "male"
         this.sex = this.sex == "male" ? "female" : "male"
+        let c = this.selClass
+        this.selClass = 0
+        this.selClass = c
     },
     toNameCase: function(str){
         return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     }
   },
   computed:{
-      currentRace: function(){
-          return this.races[this.selRace]
-      },
-      stature: function(){
-          let maxDie = this.currentRace.stature.heightWeightRoll[this.sex]
-          let roll = Math.floor( Math.random() * (maxDie-2) ) + 2
-          let heightInches = this.currentRace.stature.baseHeightInches[this.sex]+roll
-          let weight = this.currentRace.stature.baseWeight[this.sex]+( roll * this.currentRace.stature.weightMultiplyer[this.sex] )
+    currentRace: function(){
+        return this.races[this.selRace]
+    },
+    currentClass: function(){
+        if (this.selClass != this.classes.length){
+            return this.classes[this.selClass]
+        }else{
+            return this.classes[ Math.floor(Math.random()*this.classes.length-1) ]
+        }
+    },
+    stature: function(){
+        let maxDie = this.currentRace.stature.heightWeightRoll[this.sex]
+        let roll = Math.floor( Math.random() * (maxDie-2) ) + 2
+        let heightInches = this.currentRace.stature.baseHeightInches[this.sex]+roll
+        let weight = this.currentRace.stature.baseWeight[this.sex]+( roll * this.currentRace.stature.weightMultiplyer[this.sex] )
 
-          let remainder = heightInches % 12
-          let feet = (heightInches - remainder)/12
+        let remainder = heightInches % 12
+        let feet = (heightInches - remainder)/12
 
-          return {
-              heightInches: heightInches,
-              writtenHeight: feet+" ft "+remainder+'in',
-              weight: weight,
-              writtenWeight: weight+" lbs"
-          }
-      },
-      name: function(){
-          if (this.currentRace.firstNames){
-              let n = this.currentRace.firstNames[this.sex][Math.floor(Math.random()*this.currentRace.firstNames[this.sex].length-1)] +" "+ this.currentRace.surnames[Math.floor(Math.random()*this.currentRace.surnames.length-1)]
-              return this.toNameCase(n)
-          } else return "N/A"
-      }
+        return {
+            heightInches: heightInches,
+            writtenHeight: feet+" ft "+remainder+' in',
+            weight: weight,
+            writtenWeight: weight+" lbs"
+        }
+    },
+    name: function(){
+        if (this.currentRace.firstNames){
+            let n = this.currentRace.firstNames[this.sex][Math.floor(Math.random()*this.currentRace.firstNames[this.sex].length-1)] +" "+ this.currentRace.surnames[Math.floor(Math.random()*this.currentRace.surnames.length-1)]
+            return this.toNameCase(n)
+        } else return "N/A"
+    }
   }
 }
 </script>
